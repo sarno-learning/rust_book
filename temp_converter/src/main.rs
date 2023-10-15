aef80db00b35b987c0eda8415eda16272f12b624
Matt Sarno <matt.sarno@pm.me>
2023-10-14 19:01:41 -0600

    Add temp_converter project

---
+++ temp_converter/src/main.rs
@@ -0,0 +1,37 @@
+use std::io;
+
+fn main() {
+    println!("Welcome to my temperature converter.");
+
+    loop {
+        println!("\nPlease input your temperature. (20F or -6.67C)");
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line.");
+
+        let input = input.trim().to_lowercase();
+        let temp: String = input
+            .chars()
+            .filter(|c| c.is_digit(10) || *c == '-' || *c == '.')
+            .collect();
+        let temp: f64 = match temp.parse() {
+            Ok(num) => num,
+            Err(_) => break,
+        };
+
+        if input.contains('c') {
+            println!("{:.2}°F", celsius_to_fahrenheit(temp));
+        } else {
+            println!("{:.2}°C", fahrenheit_to_celsius(temp));
+        }
+    }
+}
+
+fn fahrenheit_to_celsius(f: f64) -> f64 {
+    (f - 32.0) * 5.0 / 9.0
+}
+
+fn celsius_to_fahrenheit(c: f64) -> f64 {
+    (c * 9.0 / 5.0) + 32.0
+}
